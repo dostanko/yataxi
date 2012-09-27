3f083c1e86f9646e1ad337548e3400de1f9792b1
Ira <dostanko@gmailx.com>
2012-09-27 13:28:57 +0300

    Stop server command moved to the top

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+lsof -i:4567 | awk 'NR!=1 {print $2}' | xargs kill
 cd "$( dirname "${BASH_SOURCE[0]}")"
 cd ..
 git checkout .
 git pull
-lsof -i:4567 | awk 'NR!=1 {print $2}' | xargs kill
 ruby server/api_calls.rb
