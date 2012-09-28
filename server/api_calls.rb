cedc6c230b34a5ef2e86ab59a34fc46171591307
Iryna Dastanka <Iryna_Dastanka@epam.com>
2012-09-28 14:40:54 +0300

    Make flexible and unlimited possible vesions of responces, dynamicly defined by folders in "responses" folder
    Valid request url should have next structure api/"folder"/"action"
    where folder - is folder in responses folde
    action - action from yandex api

--- server/api_calls.rb
+++ server/api_calls.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'sinatra'
 
-get '/api/:action' do 
-  action = params[:action]
-  file = File.open(settings.root + "/../responses/" + action)
+get '/api/:person/:action' do 
+  file = File.open(settings.root + "/../responses/" + params[:person] + "/" + params[:action])
   response_text = ""
   file.each {|line|
     response_text << line
   }
   response_text
+end
+
+get '/available_users' do
+   Dir.entries(settings.root + "/../responses" ).select {|entry| File.directory? File.join(settings.root + "/../responses" ,entry) and !(entry =='.' || entry == '..') }.join(",")
 end
